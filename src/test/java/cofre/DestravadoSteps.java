323fe8f737a2e002fe14b8a50ead97b777428cca
Guilherme de Lemos <guilherme.eti@gmail.com>
2018-05-22 14:59:51 -0300

    DestravadoSteps.java

---
+++ src/test/java/cofre/DestravadoSteps.java
@@ -0,0 +1,85 @@
+package cofre;
+
+import cucumber.api.PendingException;
+import cucumber.api.java.en.Given;
+import cucumber.api.java.en.Then;
+import cucumber.api.java.en.When;
+
+/**
+ *
+ * @author guilherme
+ */
+public class DestravadoSteps {
+
+    @Given("^que eu entrei com o numero (\\d+)$")
+    public void que_eu_entrei_com_o_numero(int arg1) throws Exception {
+        // Write code here that turns the phrase above into concrete actions
+        throw new PendingException();
+    }
+
+    @Given("^entrei com o numero (\\d+)$")
+    public void entrei_com_o_numero(int arg1) throws Exception {
+        // Write code here that turns the phrase above into concrete actions
+        throw new PendingException();
+    }
+
+    @When("^eu pressionei o botao ok$")
+    public void eu_pressionei_o_botao_ok() throws Exception {
+        // Write code here that turns the phrase above into concrete actions
+        throw new PendingException();
+    }
+
+    @Then("^o display deve mostrar uma mensagem vazia \"([^\"]*)\"$")
+    public void o_display_deve_mostrar_uma_mensagem_vazia(String arg1) throws Exception {
+        // Write code here that turns the phrase above into concrete actions
+        throw new PendingException();
+    }
+
+    @When("^a porta esta aberta$")
+    public void a_porta_esta_aberta() throws Exception {
+        // Write code here that turns the phrase above into concrete actions
+        throw new PendingException();
+    }
+
+    @Then("^o display deve mostrar a mensagem de porta aberta \"([^\"]*)\"$")
+    public void o_display_deve_mostrar_a_mensagem_de_porta_aberta(String arg1) throws Exception {
+        // Write code here that turns the phrase above into concrete actions
+        throw new PendingException();
+    }
+
+    @When("^a porta esta fechada$")
+    public void a_porta_esta_fechada() throws Exception {
+        // Write code here that turns the phrase above into concrete actions
+        throw new PendingException();
+    }
+
+    @Then("^o display deve mostrar o numero \"([^\"]*)\"$")
+    public void o_display_deve_mostrar_o_numero(String arg1) throws Exception {
+        // Write code here that turns the phrase above into concrete actions
+        throw new PendingException();
+    }
+
+    @Then("^o display deve mostrar a mensagem de sucesso \"([^\"]*)\"$")
+    public void o_display_deve_mostrar_a_mensagem_de_sucesso(String arg1) throws Exception {
+        // Write code here that turns the phrase above into concrete actions
+        throw new PendingException();
+    }
+
+    @Then("^o display deve mostrar a mensagem de erro de senha pequena \"([^\"]*)\"$")
+    public void o_display_deve_mostrar_a_mensagem_de_erro_de_senha_pequena(String arg1) throws Exception {
+        // Write code here that turns the phrase above into concrete actions
+        throw new PendingException();
+    }
+
+    @Given("^que eu entrei com o numero (\\d+) e (\\d+)$")
+    public void que_eu_entrei_com_o_numero_e(int arg1, int arg2) throws Exception {
+        // Write code here that turns the phrase above into concrete actions
+        throw new PendingException();
+    }
+
+    @Then("^o display deve mostrar os numeros digitados \"([^\"]*)\"$")
+    public void o_display_deve_mostrar_os_numeros_digitados(String arg1) throws Exception {
+        // Write code here that turns the phrase above into concrete actions
+        throw new PendingException();
+    }
+}
